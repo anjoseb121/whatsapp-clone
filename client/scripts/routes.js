331d22c3a58c628c5c633fc2dc5ef38c401db12c
AntonioBar <anjose.121@gmail.com>
2017-03-28 23:18:42 -0500

    Initial commit

---
+++ client/scripts/routes.js
@@ -0,0 +1,27 @@
+/**
+ * Created by HP on 3/28/2017.
+ */
+import { Config } from 'angular-ecmascript/module-helpers'
+import chatTemplateUrl from '../templates/chats.html'
+import tabsTemplateUrl from '../templates/tabs.html'
+
+export default class RoutesConfig extends Config {
+    configure() {
+        this.$stateProvider
+            .state('tab', {
+                url: '/tab',
+                abstract: true,
+                templateUrl: tabsTemplateUrl
+            })
+            .state('tab.chats', {
+                url: 'chats',
+                views: {
+                    'tab-chats': {
+                        templateUrl: chatsTemplateUrl
+                    }
+                }
+            });
+        this.$urlRouterProvider.otherwise('tab/chats')
+    }
+}
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider']
